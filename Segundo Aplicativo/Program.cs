97d4238f84f3fbe1a855eedaa9c48f20260cad08
Cassio Azevedo Cancio <42720780898@info.local>
2019-04-01 08:23:59 -0300

    Início

---
+++ Segundo Aplicativo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Segundo_Aplicativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("               Calculadora de média               ");
+
+            float primeira= 9;
+            float segunda= 10;
+            float terceira= 5;
+
+            float soma;
+
+            soma = primeira + segunda + terceira;
+
+            float media = soma/3;
+
+            Console.WriteLine("A soma das notas é: "+soma+" e a média é: "+media);
+        }
+    }
+}
