5b8a0a4af6f4448f0ac057971d7e5e1a8768b7a2
Cassio Azevedo Cancio <42720780898@info.local>
2019-04-05 11:31:57 -0300

    sextas

---
+++ Asteríscos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Asteríscos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhas = 3;
+            int colunas = 2;
+
+            for(int i= 1; i <= linhas; i++) {
+                for(int j= 1; j <= linhas; j++) {
+                    System.Console.WriteLine($"[{i},{j}]");
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
