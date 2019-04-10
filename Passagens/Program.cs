9bc8ca66cd73b587b92087d53d028876a85e942c
Cassio Azevedo Cancio <42720780898@info.local>
2019-04-10 11:27:11 -0300

    oi

--- Passagens/Program.cs
+++ Passagens/Program.cs
@@ -4,48 +4,56 @@ namespace Passagens {
     class Program {
         static void Main (string[] args) {
             bool continuar = true;
-            int numero = 0;
+            string numero;
             int i = 1;
-            int a = 1;
             string[] nome = new string[10];
-            string[] passagem = new string[10];
+            int[] passagem = new int[10];
             DateTime[] data = new DateTime[10];
+            string teste;
+            string test;
 
             Console.WriteLine ("\n--------------- Cancio's Airlines ---------------");
 
             while (continuar) {
                 Console.WriteLine ("\n Digite os seguintes números para:\n\n 1 - Registrar passagem;\n 2 - Visualizar passagens;\n 0 - Para SAIR do programa;\n");
-                numero = int.Parse (Console.ReadLine ());
+                numero = Console.ReadLine ();
                 switch (numero) {
-                    case 1:
+
+                    case "1":
                         if (i <= 5) {
-                            Console.WriteLine ("\nQual o nome do passageiro?");
+                            Console.Write ($"\nInsira o nome do {i}° passageiro: ");
                             nome[i] = Console.ReadLine ();
-                            Console.WriteLine ("\nInsira o número da passagem:");
-                            passagem[i] = Console.ReadLine ();
-                            Console.WriteLine ("\nInsira a data da viagem:");
-                            data[i] = DateTime.Parse(Console.ReadLine ());
+                            do {
+                                Console.Write ($"Insira um número válido da {i}° passagem: ");
+                                teste = Console.ReadLine ();
+                            } while (!int.TryParse (teste, out passagem[i]));
+                            do {
+                                Console.Write ($"Insira uma data válida para a {i}° viagem (dd/mm/aaaa): ");
+                                test = Console.ReadLine ();
+                            } while (!DateTime.TryParse (test, out data[i]));
                             i++;
                         } else {
                             Console.WriteLine ("\n--------------- Número máximo de compras atingido ---------------");
                         }
+                        break;
 
-                        break;
-                    case 2:
-                        a = 1;
-                        for (a = 1; a < i; a++) {
-                        Console.WriteLine ($"--------------------------------\nPassageiro: {nome[a]} \nNúmero da passagem: {passagem[a]}\nNa data: {data[a]:dd:MM:yyyy}\n--------------------------------");
+                    case "2":
+                        for (int a = 1; a < i; a++) {
+                            Console.WriteLine ($"------------------------------------------\nPassageiro {a}: {nome[a]} \nN° da passagem: {passagem[a]}\nData: {data[a]:dd/MM/yyyy}\n------------------------------------------");
                         }
+                        Console.WriteLine ("\n--------------- Clique ENTER para continuar ---------------");
+                        Console.ReadLine ();
                         break;
-                    case 0:
+
+                    case "0":
                         continuar = false;
                         break;
+
                     default:
                         Console.WriteLine ("\n--------------- Digite um valor válido ---------------");
                         break;
                 }
             }
-
         }
     }
 }
