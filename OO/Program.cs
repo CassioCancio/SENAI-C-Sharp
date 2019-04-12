c4def354779c9c11d9001a1766a39c74abf8e844
Cassio Azevedo Cancio <42720780898@info.local>
2019-04-12 11:31:05 -0300

    sexta-feira

---
+++ OO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OO
+{
+    class Usuario
+    {
+        int codigo = "1234";
+        string nome = "Cassio";
+        string curso = "Dev";
+        string senha = "1234";
+        DateTime dataNascimento;
+
+        public string  InserirSenha(string entrada){
+            return senha;
+        }
+    }
+}
---
+++ Passagens de outro modo/Program.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tsukair
+{
+    class Passageiro
+    {
+        string nome;
+        int numeroPassagem;
+        DateTime data;
+        public void setNome(string nome){
+            this.nome = nome;
+        }
+        public string getNome(){
+            return this.nome;
+        }
+        public void setNumero(int numeroPassagem){
+            this.numeroPassagem = numeroPassagem;
+        }
+        public int getNumero(){
+            return this.numeroPassagem;
+        }
+        public void setData(DateTime data){
+            this.data = data;
+        }
+        public DateTime getData(){
+            return this.data;
+        }
+    }
+}
+
